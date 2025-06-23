8e39ecd310e8da7c11d08535d03e1a5b46273bff
Usman Ahmad <usmanahmad417406@gmail.com>
2025-06-23 15:54:23 +0500

    uplaoding

---
+++ front.py
@@ -0,0 +1,45 @@
+# app.py
+import streamlit as st
+import numpy as np
+import pandas as pd
+import requests
+from io import BytesIO
+
+st.set_page_config(page_title="Transaction Scoring", layout="centered")
+st.title("📊 Transaction Scoring App")
+
+uploaded_file = st.file_uploader("Upload your Excel file (.xlsx)", type=["xlsx"])
+
+if uploaded_file:
+    st.subheader("Preview of Uploaded Data")
+    df = pd.read_excel(uploaded_file)
+    st.dataframe(df.head())
+
+    if st.button("Submit for Scoring"):
+        with st.spinner("Sending file ..."):
+            uploaded_file.seek(0)
+            response = requests.post(
+                "https://b322-182-176-168-81.ngrok-free.app",
+                files={"file": (uploaded_file.name, uploaded_file, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")}
+            )
+
+            if response.status_code == 200:
+                result = pd.DataFrame(response.json())
+                
+
+                st.success("✅ Scoring complete!")
+                st.subheader("🔍 Scored Data")
+                st.dataframe(result)
+
+                output = BytesIO()
+                with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
+                    result.to_excel(writer, index=False, sheet_name='ScoredData')
+
+                st.download_button(
+                    label="📥 Download Scored Excel",
+                    data=output.getvalue(),
+                    file_name="scored_output.xlsx",
+                    mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+                )
+            else:
+                st.error(f"❌ Error: {response.status_code} - {response.text}")
